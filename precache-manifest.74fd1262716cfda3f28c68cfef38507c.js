55cf26ce7398c670c6d107bb802d875fad3f65bd
Konstantin R <renevan@list.ru>
2020-05-26 18:19:09 +0300

    Updates

---
+++ precache-manifest.74fd1262716cfda3f28c68cfef38507c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3d498d55d918be15a14065393d4bb2",
+    "url": "/hurricane/index.html"
+  },
+  {
+    "revision": "3640d980a64f677c5236",
+    "url": "/hurricane/static/css/main.ea323a3d.chunk.css"
+  },
+  {
+    "revision": "5f6b8547e6a11ecd7eee",
+    "url": "/hurricane/static/js/2.c110ae3c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hurricane/static/js/2.c110ae3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3640d980a64f677c5236",
+    "url": "/hurricane/static/js/main.5a91db17.chunk.js"
+  },
+  {
+    "revision": "369609fd14e8f74b094e",
+    "url": "/hurricane/static/js/runtime-main.866ac60d.js"
+  },
+  {
+    "revision": "0b6f9d4276bb407ac2326a57772d9064",
+    "url": "/hurricane/static/media/2.0b6f9d42.jpg"
+  },
+  {
+    "revision": "bc25bfb4803c1d0a8b13473490d69b70",
+    "url": "/hurricane/static/media/lou.bc25bfb4.png"
+  },
+  {
+    "revision": "85ed543561bac0b598135ec569fa11ac",
+    "url": "/hurricane/static/media/spinner.85ed5435.gif"
+  }
+]);
